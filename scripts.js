531a7c17c510ee8e11a8b2d62b752493be37cf78
Paullux <Paullux@users.noreply.github.com>
2023-10-26 11:44:43 +0000

    deploy: 538a8e769dd0bb52849301951f928e4f9b21f9c7

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const area = document.getElementById('bar');
+const project = Array.from(document.querySelectorAll('.project'));
+const projectText = Array.from(document.querySelectorAll('h2'));
+const projectParagraph = Array.from(document.getElementsByClassName("paragraph")) 
+
+if (area.addEventListener) {
+    area.addEventListener('input', () => {
+        selector();
+    }, false);
+} else if (area.attachEvent) {
+    area.attachEvent('onpropertychange', () => {
+        selector();
+    });   
+}
+
+function selector() {
+    let text =  area.value.toLowerCase();
+    if (text != '') {
+        for (let i = 0; i < project.length; i++ ) {
+            if (!projectText[i].innerHTML.toLowerCase().includes(text) && !projectParagraph[i].innerHTML.toLowerCase().includes(text)) {
+                project[i].hidden = true;
+            } else {
+                project[i].hidden = false;
+            }
+        }
+    } else {
+        for (let i = 0; i < project.length ; i++) {
+            project[i].hidden = false;
+        }
+    }
+}
