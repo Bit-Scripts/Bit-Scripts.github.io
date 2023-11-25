fed79b4ff7b3c6ff7bcfb3d872109d91e30b0398
Paullux <paulwoisard@gmail.com>
2023-11-25 15:04:21 +0100

    Ajout des deux nouveaux projets développés par Red Moon

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const area = document.getElementById('bar');
 const project = Array.from(document.querySelectorAll('.project'));
+const projectImg = Array.from(document.querySelectorAll('.illustration'));
 const projectText = Array.from(document.querySelectorAll('h2'));
 const projectParagraph = Array.from(document.getElementsByClassName("paragraph")) 
 
@@ -12,6 +13,68 @@ if (area.addEventListener) {
         selector();
     });   
 }
+
+// function selector() {
+//     let text = area.value.toLowerCase();
+//     console.log("Nombre de projets:", project.length);
+//     console.log("Nombre d'illustrations:", projectImg.length);
+//     console.log("Nombre de titres:", projectText.length);
+//     console.log("Nombre de paragraphes:", projectParagraph.length);
+
+//     for (let i = 0; i < projectText.length; i++ ) {
+//         console.log(`Traitement du projet ${i}`);
+//         let projectTitle = projectText[i].innerHTML.toString().toLowerCase();
+//         let projectDesc = projectParagraph[i].innerHTML.toString().toLowerCase();
+
+//         console.log("Titre:", projectTitle);
+//         console.log("Description:", projectDesc);
+
+//         if (!projectTitle.includes(text) && !projectDesc.includes(text)) {
+//             projectImg[i].hidden = true;
+//             projectText[i].parentElement.hidden = true;
+//             projectParagraph[i].hidden = true;
+//         } else {
+//             projectImg[i].hidden = false;
+//             projectText[i].hidden = false;
+//             projectParagraph[i].hidden = false;
+//         }
+//     }
+
+//     if (text === '') {
+//         for (let i = 0; i < project.length ; i++) {
+//             projectImg[i].hidden = false;
+//             projectText[i].hidden = false;
+//             projectParagraph[i].hidden = false;
+//         }
+//     }
+// }
+
+
+// function selector() {
+//     let text = area.value.toLowerCase();
+//     console.log("Nombre de projets:", project.length);
+//     console.log("Nombre d'illustrations:", projectImg.length);
+//     console.log("Nombre de titres:", projectText.length);
+//     console.log("Nombre de paragraphes:", projectParagraph.length);
+//     for (let i = 0; i < projectText.length; i++) {
+//         let projectTitleElement = projectText[i]; // Element DOM du titre
+//         let projectDescElement = projectParagraph[i]; // Element DOM du paragraphe
+//         let projectContainer = projectTitleElement.parentElement; // Trouver le conteneur .project
+//         console.log("Container:", projectContainer);
+
+//         let projectTitle = projectTitleElement.innerHTML.toLowerCase();
+//         let projectDesc = projectDescElement.innerHTML.toLowerCase();
+
+//         projectContainer.style.display = (!projectTitle.includes(text) && !projectDesc.includes(text)) ? 'none' : '';
+//     }
+//     if (text === '') {
+//         for (let i = 0; i < projectText.length ; i++) {
+//             let projectTitleElement = projectText[i]; // Element DOM du titre
+//             let projectContainer = projectTitleElement.parentElement; // Trouver le conteneur .project
+//             projectContainer.style.display = (!projectTitle.includes(text) && !projectDesc.includes(text)) ? 'none' : '';
+//         }
+//     }
+// }
 
 function selector() {
     let text =  area.value.toLowerCase();
